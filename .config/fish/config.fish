e0c388469e9ddfb115d3f0cadfac79a4a4bd26a6
Peter Siegel <psiegel2000@icloud.com>
2024-10-30 02:00:23 +0100

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+set -x SSH_AUTH_SOCK $HOME/.1password/agent.sock
+
+fish_add_path $HOME/.local/bin
+
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+# pnpm
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/psiegel/miniconda3/bin/conda
+    status is-interactive && eval /home/psiegel/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/psiegel/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/psiegel/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/psiegel/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
