d4b1b7832e941dbe2aa338053d2431afafc3bfe3
Peter Siegel <psiegel2000@icloud.com>
2025-01-08 17:02:05 +0100

    feat: export SSH_AUTH_SOCK

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,10 @@ direnv hook fish | source
 
 alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
+if not set -q SSH_TTY
+    set -x SSH_AUTH_SOCK ~/.1password/agent.sock
+end
+
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
