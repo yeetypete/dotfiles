fac9365b9af1b2b054cd0f4b890b075d733705c3
Peter Siegel <psiegel2000@icloud.com>
2025-05-24 23:10:04 +0200

    fix: tailscale status autocomplete

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,14 @@ if not set -q SSH_CONNECTION
     set -x SSH_AUTH_SOCK ~/.1password/agent.sock
 end
 
+function fzf_tailscale_nodes
+    set node (tailscale status | awk '!/^#/ && $2 != "" {print $2}' | fzf)
+    if test -n "$node"
+        commandline -i "$node"
+    end
+end
+bind \cs fzf_tailscale_nodes
+
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
